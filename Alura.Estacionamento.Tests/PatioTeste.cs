84ba81cea2e57a806d217c5165469ad988b50d9e
José Guilherme de Lima Shirotori <joseguilhermeshirotori@hotmail.com>
2024-01-20 12:42:41 -0300

    Primeiro commit

---
+++ Alura.Estacionamento.Tests/PatioTeste.cs
@@ -0,0 +1,38 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.Estacionamento.Tests
+{
+    public class PatioTeste
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo() 
+            { 
+                Proprietario = "José Guilherme",
+                Tipo = TipoVeiculo.Automovel,
+                Cor = "Verde",
+                Modelo = "Fusca",
+                Placa = "asd-9999"
+            };
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+        }
+    }
+}
---
+++ Alura.Estacionamento.Tests/VeiculoTeste.cs
@@ -0,0 +1,38 @@
+using Alura.Estacionamento.Modelos;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Alura.Estacionamento.Tests
+{
+    public class VeiculoTeste
+    {
+       
+        [Fact]   
+        public void TestaVeiculoAcelerar()
+        {
+            //Arrange
+           var veiculo = new Veiculo();
+
+            //Act
+            veiculo.Acelerar(10);
+
+            //Assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+
+        }
+
+        [Fact]        
+        public void TestaVeiculoFrear()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+            veiculo.Frear(10);
+            //Assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+        
+    }
+}
